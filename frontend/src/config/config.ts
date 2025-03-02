6b1566948c4f3c18270cc96293c274defdaa3e89
sugamkuber <sugam7842692@gmail.com>
2025-03-02 16:26:48 +0530

    fixes

--- frontend/src/config/config.ts
+++ frontend/src/config/config.ts
@@ -1,1 +1,9 @@
-export const API_URL = 'http://localhost:5000/api';
+const DEV_API_URL = 'http://localhost:5000/api';
+const PROD_API_URL = 'https://globetrotte.api.sugam.cloud/api';
+
+const FRONTEND_DOMAIN = window.location.origin;
+
+// Use DEV_API_URL if running on localhost, otherwise use PROD_API_URL
+export const API_URL = FRONTEND_DOMAIN.includes("localhost") ? DEV_API_URL : PROD_API_URL;
+
+console.log("Using API URL:", API_URL);
